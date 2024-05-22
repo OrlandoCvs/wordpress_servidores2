5c406b73a3336b4287469c5f6c112c0319888e67
OrlandoCvs <112524194+OrlandoCvs@users.noreply.github.com>
2024-05-22 01:35:35 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'wordpress_ctyb' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'slchLvP2T8H96SQqT6fEZnIlfYeh6lmY' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'dpg-cp6psm20si5c73ai8qrg-a' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
